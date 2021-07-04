6d05e8baa7176c28ebc847a70bc18da07822170b
Socrates <liuzhcloud@gmail.com>
2021-07-04 10:53:54 +0800

    update directory structure

--- index.md
+++ index.md
@@ -1,10 +1,3 @@
 ---
 home: true
-features:
-  - title: 躺平至上
-    details: 以 躺平 为中心的项目结构，以最懒的姿势帮助你专注于写作。
-  - title: Money 驱动
-    details: 享受 Money 的收入体验，可以在 躺平 中获得 Money，又可以使用 Money 来继续躺平。
-  - title: 葛优躺
-    details: 会为每个页面预渲染生成静态的躺平姿势，同时，每个页面被加载的时候，将以 葛优躺 运行。
 ---
--- vuepress.config.ts
+++ vuepress.config.ts
@@ -5,11 +5,19 @@ export default defineUserConfig<DefaultThemeOptions>({
   lang: 'zh-CN',
   title: '苏格拉底儿',
   description: '破茧成蝶，羽化成仙',
-
+  head: [
+    ['script', { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=UA-201208347-1' }],
+    ['script', {}, `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-201208347-1');
+    `]
+  ],
+  public: 'public',
   themeConfig: {
     darkMode: true,
     sidebar: 'auto',
-
     navbar: [
       {
         text: 'Windows',
